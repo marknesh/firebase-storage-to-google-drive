9723a8f35ac388f81cfbcb57cb14cfefdaf2cf1f
marknesh <markmunene.nesh@gmail.com>
2025-07-09 22:54:10 +0300

    set google auth client and google drive files fetch

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Storage } from "@google-cloud/storage";
 import { config } from "dotenv";
+import { google } from "googleapis";
 
 config();
 
@@ -10,9 +11,40 @@ async function uploadFile() {
   return await storage.bucket(bucketName).getFiles();
 }
 
-uploadFile()
-  .then((res) => console.log(res[2]))
-  .catch((err) => {
-    console.error("❌ Upload failed:", err);
-    process.exit(1);
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
+
+/**
+ * Authorize with default service account and get the JWT client
+ *
+ * @return {JWT} jwtClient
+ *
+ */
+async function authorize() {
+  const JWTClient = await google.auth.getClient({
+    scopes: SCOPES,
+    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   });
+
+  return JWTClient;
+}
+
+authorize()
+  .then(async (result) => {
+    const drive = google.drive({ version: "v3", auth: result });
+    console.log(result);
+    const response = await drive.files.list({
+      fields: "files(id, name, md5Checksum)",
+    });
+
+    console.log(response?.data.files?.[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// uploadFile()
+//   .then((res) => console.log(res[2]))
+//   .catch((err) => {
+//     console.error("❌ Upload failed:", err);
+//     process.exit(1);
+//   });
