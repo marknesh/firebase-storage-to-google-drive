bb441d7f4805290de3a7c145f12bb762d9cdd0d8
marknesh <markmunene.nesh@gmail.com>
2025-07-09 22:18:16 +0300

    add .env to gitignore

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
+import { Storage } from "@google-cloud/storage";
+import { config } from "dotenv";
 
+config();
+
+const bucketName = process.env.BUCKET_NAME as string;
+
+async function uploadFile() {
+  const storage = new Storage();
+  return await storage.bucket(bucketName).getFiles();
+}
+
+uploadFile()
+  .then((res) => console.log(res[2]))
+  .catch((err) => {
+    console.error("❌ Upload failed:", err);
+    process.exit(1);
+  });
